5eca8dea05cdc5e51ba919fa2583163f13d16423
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2018-01-17 23:40:49 +0530

    markers interface setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  // Start Postition
+  zoom = 10;
+  lat = 28.7041;
+  lng = 77.1025;
+  markers: Marker[] = [
+    {
+      name: 'Uttar Pradesh',
+      lat: 26.8467,
+      lan: 80.9462
+    },
+    {
+      name: 'Punjab',
+      lat: 31.1471,
+      lan: 75.3412
+    },
+    {
+      name: 'Kerala',
+      lat: 10.8505,
+      lan: 76.2711
+    }
+  ];
+
+  constructor() {}
+
+}
+
+interface Marker {
+  name?: string;
+  lat: number;
+  lan: number;
+  draggable?: boolean;
 }
